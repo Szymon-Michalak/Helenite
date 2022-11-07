197c29aa25b97bc3e05bd10027c3e9a9c51f1967
Szymon Michalak <sz.p.michalak@gmail.com>
2022-11-07 17:19:59 +0100

    Fix mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,10 +5,36 @@ import NoteList from '../components/note-list'
 import Pager from '../components/pager'
 import '../styles/about.css'
 import '../styles/indexmobile.css'
+import {useEffect, useState} from 'react';
+
+const getWidth = () => window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+
+const useWindowWidth = () => {
+  let [width, setWidth] = useState(getWidth());
+
+  useEffect(() => {
+    let timeoutId = null;
+    const resizeListener = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+    };
+    window.addEventListener('resize', resizeListener);
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
+
+  return width;
+};
 
 export default function About({ pageContext, data }) {
   console.log(pageContext);
-  const isMobile = window.innerWidth <= 500;
+  let currentWidth;
+  if (typeof window !== 'undefined') currentWidth = useWindowWidth()
+  else currentWidth = 700;
+  let isMobile = currentWidth <= 500;
   if (isMobile)
   return (
     <Layout>
